ada7cac4e8fb7b3b64719db9d8b36e2d826cf91b
Oscar Lagatta <oscarlagatta@gmail.com>
2024-10-29 23:00:10 +0000

    Adding shadcn to the home improvements

--- apps/home-improvement-hub/src/app/app.tsx
+++ apps/home-improvement-hub/src/app/app.tsx
@@ -1,9 +1,37 @@
-import NxWelcome from './nx-welcome';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+  Alert, AlertDescription,
+  AlertTitle
+} from '@home-improvement-hub/shadcn';
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="home-improvement-hub" />
+    <div className="container mx-auto px-4">
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <Alert>
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+      <Alert variant="destructive">
+        <ExclamationTriangleIcon className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>
+          Your session has expired. Please log in again.
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
