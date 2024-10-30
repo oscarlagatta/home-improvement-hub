ec0bef3f379cc431e772bb5a284e3bc4a9144a37
Moshe-Victor Solomon <moshevictor@yahoo.co.uk>
2024-10-30 23:24:59 +0000

    Adding playground app

---
+++ apps/home-improvement-playground/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="home-improvement-playground" />
+    </div>
+  );
+}
+
+export default App;
