9893fe714f23b25f23beab5bd8757ed7c5285d7a
Moshe-Victor Solomon <moshevictor@yahoo.co.uk>
2024-10-31 22:54:16 +0000

    Adding shadcn components.

--- apps/home-improvement-hub/tailwind.config.js
+++ apps/home-improvement-hub/tailwind.config.js
@@ -77,4 +77,17 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
+  // theme: {
+  //   extend: {
+  //     keyframes: {
+  //       "caret-blink": {
+  //         "0%,70%,100%": { opacity: "1" },
+  //         "20%,50%": { opacity: "0" },
+  //       },
+  //     },
+  //     animation: {
+  //       "caret-blink": "caret-blink 1.25s ease-out infinite",
+  //     },
+  //   },
+  // },
 };
