06062d83467f1d807187d4df65114458a3e5d085
ALAM <alam.2008@live.co.uk>
2024-11-01 19:40:56 +0000

    created feature-booking and feature-services libraries

---
+++ libs/feature-bookings/src/lib/feature-bookings.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledFeatureBookings = styled.div`
+  color: pink;
+`;
+
+export function FeatureBookings() {
+  return (
+    <StyledFeatureBookings>
+      <h1>Welcome to FeatureBookings!</h1>
+    </StyledFeatureBookings>
+  );
+}
+
+export default FeatureBookings;
---
+++ libs/feature-services/src/lib/feature-services.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledFeatureServices = styled.div`
+  color: pink;
+`;
+
+export function FeatureServices() {
+  return (
+    <StyledFeatureServices>
+      <h1>Welcome to FeatureServices!</h1>
+    </StyledFeatureServices>
+  );
+}
+
+export default FeatureServices;
