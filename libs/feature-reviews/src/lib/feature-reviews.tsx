5f0b622b5bb6f0bae6ef58853b152d67ab7557e4
ALAM <alam.2008@live.co.uk>
2024-11-01 20:10:18 +0000

    created feature-reviews library

---
+++ libs/feature-reviews/src/lib/feature-reviews.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledFeatureReviews = styled.div`
+  color: pink;
+`;
+
+export function FeatureReviews() {
+  return (
+    <StyledFeatureReviews>
+      <h1>Welcome to FeatureReviews!</h1>
+    </StyledFeatureReviews>
+  );
+}
+
+export default FeatureReviews;
