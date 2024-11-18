ea0f493f28ae428537317740271d6b0a8cf98212
alamuk <alam.2008@live.co.uk>
2024-11-18 00:07:44 +0000

    dashboard-libs-updated

--- libs/dashboard/dashboard-page/src/lib/dashboard-page.tsx
+++
@@ -1,5 +0,0 @@
-export function DashboardPage() {
-  return <h1>Welcome to DashboardPage!</h1>;
-}
-
-export default DashboardPage;
---
+++ libs/dashboard/dashboard-ui/src/lib/dashboard-ui.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledDashboardUi = styled.div`
+  color: pink;
+`;
+
+export function DashboardUi() {
+  return (
+    <StyledDashboardUi>
+      <h1>Welcome to DashboardUi!</h1>
+    </StyledDashboardUi>
+  );
+}
+
+export default DashboardUi;
