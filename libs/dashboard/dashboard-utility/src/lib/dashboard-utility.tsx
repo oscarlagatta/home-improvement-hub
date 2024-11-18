8cbff364c3373c5c5daf6b2de7fa186ebd81f70d
alamuk <alam.2008@live.co.uk>
2024-11-18 00:19:14 +0000

    dashboard-libs-updated

---
+++ libs/dashboard/dashboard-utility/src/lib/dashboard-utility.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledDashboardUtility = styled.div`
+  color: pink;
+`;
+
+export function DashboardUtility() {
+  return (
+    <StyledDashboardUtility>
+      <h1>Welcome to DashboardUtility!</h1>
+    </StyledDashboardUtility>
+  );
+}
+
+export default DashboardUtility;
